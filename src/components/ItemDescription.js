e1a577631ec188519a2eb408ce1ee04e146eeb0a
pat shiu <shiu.patricia@gmail.com>
2022-07-08 20:53:55 -0400

    Restoring ItemDescription component

--- src/components/ItemDescription.js
+++ src/components/ItemDescription.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './ItemDescription.scss';
-
 const ItemDescription = ({ description }) => {
   return <div dangerouslySetInnerHTML={{ __html: description }} />;
 };
