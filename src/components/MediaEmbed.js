c63920db9b8df2edb1ee059ebadb9710b1c36b36
pat shiu <shiu.patricia@gmail.com>
2022-07-09 16:34:36 -0400

    Simplifying switch statment

--- src/components/MediaEmbed.js
+++ src/components/MediaEmbed.js
@@ -50,35 +50,17 @@ MediaEmbed.propTypes = {
 const mediaTypeHasEmbed = (mediaType) => {
   switch (mediaType) {
     case 'texts':
-      return true;
-
     case 'etree':
-      return true;
-
     case 'audio':
-      return true;
-
     case 'movies':
-      return true;
-
     case 'software':
-      return true;
-
     case 'image':
       return true;
 
     case 'data':
-      return false;
-
     case 'web':
-      return false;
-
     case 'collection':
-      return false;
-
     case 'account':
-      return false;
-
     default:
       return false;
   }
