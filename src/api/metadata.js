80c1017b3bb664e952af84166aa7c727781aa0bb
pat shiu <shiu.patricia@gmail.com>
2022-07-10 19:32:59 -0400

    Adding 200 status check and clearer error message for aborted request

--- src/api/metadata.js
+++ src/api/metadata.js
@@ -4,8 +4,20 @@ const fetchMetadata = (itemIdentifier, controller) => {
   const url = 'https://archive.org/metadata/' + itemIdentifier;
   return axios
     .get(url, { signal: controller.signal })
-    .then((res) => res.data)
-    .catch((e) => console.log('An error occurred while fetching item metadata:', e));
+    .then((res) => {
+      if (res.status === 200) {
+        return res.data;
+      } else {
+        throw new Error('Failed to fetch item metadata.');
+      }
+    })
+    .catch((e) => {
+      if (axios.isCancel(e)) {
+        console.log('Request for item metadata cancelled.');
+      } else {
+        console.log('An error occurred while fetching item metadata:', e);
+      }
+    });
 };
 
 export default fetchMetadata;
--- src/api/relatedItems.js
+++ src/api/relatedItems.js
@@ -4,8 +4,20 @@ const fetchRelated = (itemIdentifier, controller) => {
   const url = 'https://be-api.us.archive.org/mds/v1/get_related/all/' + itemIdentifier;
   return axios
     .get(url, { signal: controller.signal })
-    .then((res) => res.data.hits.hits)
-    .catch((e) => console.log('An error occurred while fetching related item:', e));
+    .then((res) => {
+      if (res.status === 200) {
+        return res.data.hits.hits;
+      } else {
+        throw new Error('Failed to fetch related items');
+      }
+    })
+    .catch((e) => {
+      if (axios.isCancel(e)) {
+        console.log('Request for related items cancelled.');
+      } else {
+        console.log('An error occurred while fetching related items:', e);
+      }
+    });
 };
 
 export default fetchRelated;
