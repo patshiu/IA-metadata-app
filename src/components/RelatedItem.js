ed1f74eb1239b533f92bd0e10f7245b0280a8e1d
pat shiu <shiu.patricia@gmail.com>
2022-07-10 13:09:23 -0400

    Fix accessibility issue missing alt-text

--- src/components/RelatedItem.js
+++ src/components/RelatedItem.js
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import './RelatedItem.scss';
+import ItemTitle from './ItemTitle';
 
 const RelatedItem = ({ itemSource, itemIdentifier }) => {
   return (
     <div className="related-item-card-container col-1-3 padded">
       <div className="related-item-card">
-        <img src={'https://archive.org/services/img/' + itemIdentifier} className="card-hero" />
+        <img
+          src={'https://archive.org/services/img/' + itemIdentifier}
+          className="card-hero"
+          alt-text={'thumbnail for ' + ItemTitle}
+        />
         <div className="card-content">
           <h3>
             <Link to={'/' + itemIdentifier}>{itemSource.title}</Link>
