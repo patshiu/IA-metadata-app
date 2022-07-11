6556f7cfcf2f25a3b1ea4111a6e6c8521e795734
pat shiu <shiu.patricia@gmail.com>
2022-07-10 21:23:09 -0400

    Refactoring to api calls to use async...await for consistency.

--- src/api/metadata.js
+++ src/api/metadata.js
@@ -1,29 +1,25 @@
 const fetchMetadata = async (itemIdentifier, controller) => {
   const url = 'https://archive.org/metadata/' + itemIdentifier;
-  return fetch(url, {
-    signal: controller.signal,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  })
-    .then((res) => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error('Failed to fetchh item metadata.');
-      }
-    })
-    .then((jsonRes) => {
-      return jsonRes;
-    })
-    .catch((e) => {
-      if (e.name === 'AbortError') {
-        console.log('Request for item metadata cancelled.');
-      } else {
-        console.log('An error occurred while fetching item metadata:', e);
+  try {
+    const res = await fetch(url, {
+      signal: controller.signal,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
       }
     });
+    if (res.status === 200) {
+      return await res.json();
+    } else {
+      throw new Error('Failed to fetchh item metadata.');
+    }
+  } catch (e) {
+    if (e.name === 'AbortError') {
+      console.log('Request for item metadata cancelled.');
+    } else {
+      console.log('An error occurred while fetching item metadata:', e);
+    }
+  }
 };
 
 export default fetchMetadata;
--- src/api/relatedItems.js
+++ src/api/relatedItems.js
@@ -1,29 +1,27 @@
 const fetchRelated = async (itemIdentifier, controller) => {
   const url = 'https://be-api.us.archive.org/mds/v1/get_related/all/' + itemIdentifier;
-  return fetch(url, {
-    signal: controller.signal,
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  })
-    .then((res) => {
-      if (res.status === 200) {
-        return res.json();
-      } else {
-        throw new Error('Failed to fetch related items.');
-      }
-    })
-    .then((jsonRes) => {
-      return jsonRes.hits.hits;
-    })
-    .catch((e) => {
-      if (e.name === 'AbortError') {
-        console.log('Request for related items cancelled.');
-      } else {
-        console.log('An error occurred while fetching related items:', e);
+
+  try {
+    const res = await fetch(url, {
+      signal: controller.signal,
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
       }
     });
+    if (res.status === 200) {
+      const resJson = await res.json();
+      return resJson.hits.hits;
+    } else {
+      throw new Error('Failed to fetch related items.');
+    }
+  } catch (e) {
+    if (e.name === 'AbortError') {
+      console.log('Request for related items cancelled.');
+    } else {
+      console.log('An error occurred while fetching related items:', e);
+    }
+  }
 };
 
 export default fetchRelated;
